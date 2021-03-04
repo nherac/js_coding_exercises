3831334536d986b1372321d8c77cccbb510da9db
nherac <carmen@gamezlara.com>
2021-03-04 12:38:23 +0000

    001 Exercise01

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,44 +1,92 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  result = word;
+  var firstLetter = word.charAt(0);
+  var pattern = new RegExp("[a-z]");
+  if(pattern.test(firstLetter)){
+    result = firstLetter.toUpperCase() + word.slice(1);
+  }
+  return result;
 }
+
+
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstLetterToUpperCase(firstName) + "." +firstLetterToUpperCase(lastName);
+
+}
+
+function firstLetterToUpperCase(word){
+  return word.charAt(0).toUpperCase();
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  var result = (vatRate + 100)*(originalPrice/100);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  if (reduction>=100) throw new Error("Reduction causes a negative price");
+  var result = (100-reduction)*(originalPrice/100);
+  return result;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  if (isEvenString(str)) throw new Error("The string is even");
+  var middleIndex = str.length/2;
+  return str.charAt(middleIndex);
+
+}
+
+function isEvenString(str){
+  return str.length % 2 == 0;
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  var result = "";
+  var lastIndex = word.length-1;
+  var FIRST_INDEX=0;
+  for(var i = lastIndex;i>=FIRST_INDEX;i--){
+    result = result + word[i];
+  }
+  return result;
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  if (!words.isArray()) throw new Error("words is not an array");
+  var lastIndex = word.length-1;
+  var FIRST_INDEX=0;
+  var result =[];
+  for(var i=FIRST_INDEX;i<lastIndex;i++){
+    var newWord =reverseWord( words[i]);
+    result.push(newWord);
+  }
+  return result;
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
+  if (!users.isArray()) throw new Error("Users is not an array");
+  var numberOfLinuxUsers = 0;
+  var lastIndex = word.length-1;
+  var FIRST_INDEX=0;
+  for(var i=FIRST_INDEX;i<lastIndex;i++){
+    if(users[i].type=="Linux"){
+      numberOfLinuxUsers++;
+    }
+
+  }
+  return numberOfLinuxUsers;
+
 }
 
 function getMeanScore(scores) {
@@ -48,10 +96,12 @@ function getMeanScore(scores) {
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
+  if (n<=0) throw new Error("n should be greater than 0");
+
+  
 }
 
-module.exports = {
+export default {
   capitalize,
   generateInitials,
   addVAT,
