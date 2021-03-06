493c3ade785e86ef72d12826adfa9bc4b110ef17
nherac <carmen@gamezlara.com>
2021-03-06 16:42:39 +0000

    Exercise004. Using functions. Still pending to clean the code. Tests passed.

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,72 +1,69 @@
+const {
+  customizeRound,
+} = require("../challenges/exercise001");
+
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   if (!Array.isArray(nums)) throw Error ("nums is not an Array");
-  var smallThanOneNums = [];
-  for(var i=0;i<nums.length;i++){
-    var numberIsSmallerThanOne = (nums[i]<1);
-    if(numberIsSmallerThanOne){
-      smallThanOneNums.push(nums[i]);
-    }
-  }
-  return smallThanOneNums;
-
+  return nums.filter(num => num<1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   if (!Array.isArray(names)) throw Error("names is not an array");
-  var result = [];
-  for(var i=0;i<names.length;i++){
-    var nameStartsWithChar = names[i].charAt(0) === char;
-    if(nameStartsWithChar){
-      result.push(names[i]);
-    }
-  }
-  return result;
+  return names.filter(name => name.charAt(0) === char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   if(!Array.isArray(words)) throw new Error ("words is not an Array");
-  var result = [];
-  for(var i=0;i<words.length;i++){
-    var word = words[i];
-    var isStringContent = ((typeof word === "string" || word instanceof String));
-    if(isStringContent){
-      var isAVerb = (word.slice(0,3)=== "to ");
-      if(isAVerb){
-        result.push(word);
-      }
-    }
-  }
-  return result;
+  return  words.filter(word => word.slice(0,3)=== "to ");
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.filter(num => isIntegerNumber(num));
+}
+
+function isIntegerNumber(number){
+ return (number === Math.floor(number));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
+  var cities = [];
+  users.forEach(user =>cities.push(getCity(user)));
+  return cities;
+}
+
+function getCity(userParam) {
+  return userParam.data.city.displayName;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  var result = [];
+  nums.forEach(num =>result.push(customizeRound(Math.sqrt(num),2)));
+  return result;
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str.toLowerCase()));
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  var result = [];
+  triangles.forEach(triangle =>result.push(getLongestSide(triangle)));
+  return result;
+}
+
+function getLongestSide(arrayA){
+  return Math.max.apply(null,arrayA);
 }
 
 module.exports = {
