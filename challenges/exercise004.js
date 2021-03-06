06228e542bbe230c2169c6951f40758eb93ecda5
nherac <carmen@gamezlara.com>
2021-03-06 10:32:27 +0000

    Exercise004. Using for loops to filter items. Customize depening on function. The code is repeating the same.

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,17 +1,46 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  if (!Array.isArray(nums)) throw Error ("nums is not an Array");
+  var smallThanOneNums = [];
+  for(var i=0;i<nums.length;i++){
+    var numberIsSmallerThanOne = (nums[i]<1);
+    if(numberIsSmallerThanOne){
+      smallThanOneNums.push(nums[i]);
+    }
+  }
+  return smallThanOneNums;
+
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  if (!Array.isArray(names)) throw Error("names is not an array");
+  var result = [];
+  for(var i=0;i<names.length;i++){
+    var nameStartsWithChar = names[i].charAt(0) === char;
+    if(nameStartsWithChar){
+      result.push(names[i]);
+    }
+  }
+  return result;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  if(!Array.isArray(words)) throw new Error ("words is not an Array");
+  var result = [];
+  for(var i=0;i<words.length;i++){
+    var word = words[i];
+    var isStringContent = ((typeof word === "string" || word instanceof String));
+    if(isStringContent){
+      var isAVerb = (word.slice(0,3)=== "to ");
+      if(isAVerb){
+        result.push(word);
+      }
+    }
+  }
+  return result;
 }
 
 function getIntegers(nums) {
